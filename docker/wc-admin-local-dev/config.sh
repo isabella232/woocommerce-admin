cdebd925c5f5f8b2f3aa4272c648146cec5bee10
Moon <moon.kyong@automattic.com>
2021-01-28 19:58:44 -0800

    Add local-dev:config cmd

--- docker/wc-admin-local-dev/config.sh
+++ docker/wc-admin-local-dev/config.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+BASEDIR=$(dirname "$0")
+
 replace() {
     find=$1
     replace=$2
@@ -16,21 +18,21 @@ insertAtFirstLine() {
     mv "$2.tmp" "$2"
 }
 
-cp Dockerfile.sample Dockerfile
+cp "$BASEDIR/Dockerfile.sample" "$BASEDIR/Dockerfile"
 
 PS3='Choose your PHP version: '
 versions=("PHP 7.4" "PHP 8.0")
 select version in "${versions[@]}"; do
     case $version in
         "PHP 7.4")
-            deleteLineStartingWith "FROM" "Dockerfile"
-            insertAtFirstLine "FROM php:7.4-fpm" "Dockerfile"
+            deleteLineStartingWith "FROM" "$BASEDIR/Dockerfile"
+            insertAtFirstLine "FROM php:7.4-fpm" "$BASEDIR/Dockerfile"
             break
         # optionally call a function or run some code here
             ;;
         "PHP 8.0")
-            deleteLineStartingWith "FROM" "Dockerfile"
-            insertAtFirstLine "FROM php:8.0-fpm" "Dockerfile"
+            deleteLineStartingWith "FROM" "$BASEDIR/Dockerfile"
+            insertAtFirstLine "FROM php:8.0-fpm" "$BASEDIR/Dockerfile"
             break
         # optionally call a function or run some code here
             ;;
@@ -39,17 +41,17 @@ done
 
 [ -d mysql_data ] || mkdir mysql_data
 
-cp Makefile.sample Makefile
-replace ":service" "${PWD##*/}" "Makefile"
+cp "$BASEDIR/Makefile.sample" "$BASEDIR/Makefile"
+replace ":service" "${PWD##*/}" "$BASEDIR/Makefile"
 
-cp sample.env .env
+cp "$BASEDIR/sample.env" "$BASEDIR/.env"
 
 read -p "Nginx Port (default: 8888): " nginx_port
 nginx_port=${nginx_port:-8888}
-replace ":nginx_port" "$nginx_port" ".env"
+replace ":nginx_port" "$nginx_port" "$BASEDIR/.env"
 
 read -p "Mysql Port (default: 3307): " mysql_port
 mysql_port=${mysql_port:-3307}
-replace ":mysql_port" "$mysql_port" ".env"
+replace ":mysql_port" "$mysql_port" "$BASEDIR/.env"
 
-chmod -R +x  recipes
+chmod -R +x  "$BASEDIR/recipes"
